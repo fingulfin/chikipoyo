47154a3b9a62378ea8fc387e43423d41351c6f32
Juan Carlos <rapidclimate@yahoo.com>
2026-08-17 08:34:53 -0600

    tabs

--- src/app/domains/admin/modules/maestros/features/administracion/administracion.ts
+++ src/app/domains/admin/modules/maestros/features/administracion/administracion.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCard } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'maestros-administracion',
-  imports: [MatCard],
+  imports: [MatCard, MatTabsModule],
   templateUrl: './administracion.html',
 })
 export default class MaestrosAdministracion {}
